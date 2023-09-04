5000a7073a5fa5570b73d73b32cdcc5ca6a733c9
HBIbidunni <ayodunibidunni@gmail.com>
2023-09-04 22:34:26 +0000

    adding the required header folder

--- src/upng.c
+++ src/upng.c
@@ -29,7 +29,7 @@
 #include <string.h>
 #include <limits.h>
 
-#include "../inc/maze.h/upng.h"
+#include "../headers/maze.h/upng.h"
 
 #define MAKE_BYTE(b) ((b) & 0xFF)
 #define MAKE_DWORD(a,b,c,d) ((MAKE_BYTE(a) << 24) | (MAKE_BYTE(b) << 16) | (MAKE_BYTE(c) << 8) | MAKE_BYTE(d))
